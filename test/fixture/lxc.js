33f1c01e71dbb62a171e26e6ed49cf1cee385dcc
Vincent Viallet <vincent@wiredcraft.com>
2013-03-08 18:28:51 +0800

    Use lxc external helper

---
+++ test/fixture/lxc.js
@@ -0,0 +1,41 @@
+var _ = require('underscore');
+var exec = require('child_process').exec;
+
+var template = 'ubuntu';
+var ipBase = '10.0.3.';
+var ipStart = 4;
+
+exports.servers = []
+
+// Helper functions
+exports.buildServer = function buildServer(name, callback) {
+    exec('sudo lxc-create -t '+ template +' -n '+ name, function() {
+        // define specific IP address for the new server
+        var ip = ipBase +''+ ipStart++
+        exports.servers.push({
+            name: name,
+            ip: ip
+        });
+        exec('echo "lxc.network.ipv4 = '+ ip +'/24" | sudo tee -a /var/lib/lxc/'+ name +'/config', function() {
+            exec('sudo lxc-start -d -n '+ name, callback);
+        });
+    });
+}
+
+exports.destroyServer = function destroyServer(name, callback) {
+    // hardcore destroy the box
+    exec('sudo lxc-destroy -f -n '+ name, function() {
+        // wait a short while
+        setTimeout(callback, 2000);
+    });
+}
+
+// Destroy all the created servers.
+exports.destroyServers = function destroyServers(callback) {
+    var remaining = exports.servers.length;
+    _.each(exports.servers, function(server) {
+        destroyServer(server.name, function() {
+            if (--remaining === 0) return callback();
+        });
+    });
+}
--- test/ping.mocha.js
+++ test/ping.mocha.js
@@ -4,73 +4,27 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 var path = require('path');
 
-var serversPath = path.join(__dirname, 'fixture', 'servers');
-var templatesPath = path.join(__dirname, 'fixture', 'templates');
-
-// lxc default settings - only work in linux! no macosx
-var servers = [];
-var lxcTemplate = 'ubuntu';
-var lxcIpBase = '10.0.3.';
-var lxcIpStart = 4;
-
-
+// Add lxc helper
+var lxc = require('./fixture/lxc.js');
 
 describe('spawn a box', function() {
     before(function(done) {
         this.timeout(100000);
-        buildServer('test', function() {
-            buildServer('test2', done);
+        lxc.buildServer('test', function() {
+            lxc.buildServer('test2', done);
         });
     });
     after(function(done) {
         this.timeout(100000);
-        destroyServers(done);
+        lxc.destroyServers(done);
     });
     
     it('should be able to ping both servers', function(done) {
-        var remaining = servers.length;
-        _.each(servers, function(server) {
+        var remaining = lxc.servers.length;
+        _.each(lxc.servers, function(server) {
             exec('ping -c 1 '+ server.ip, function(err, stdout, stderr) {
-                should.not.exist(err);
-                should.exist(stderr);
-                should.exist(stdout);
-                console.log(stdout);
                 if (--remaining === 0) return done();
             });
         });
     });
 });
-
-
-// Helper functions
-function buildServer(name, callback) {
-    exec('sudo lxc-create -t '+ lxcTemplate +' -n '+ name, function() {
-        // define specific IP address for the new server
-        var ip = lxcIpBase +''+ lxcIpStart++
-        servers.push({
-            name: name,
-            ip: ip
-        });
-        exec('echo "lxc.network.ipv4 = '+ ip +'/24" | sudo tee -a /var/lib/lxc/'+ name +'/config', function() {
-            exec('sudo lxc-start -d -n '+ name, callback);
-        });
-    });
-}
-
-function destroyServer(name, callback) {
-    // hardcore destroy the box
-    exec('sudo lxc-destroy -f -n '+ name, function() {
-        // wait a short while
-        setTimeout(callback, 2000);
-    });
-}
-
-// Destroy all the created servers.
-function destroyServers(callback) {
-    var remaining = servers.length;
-    _.each(servers, function(server) {
-        destroyServer(server.name, function() {
-            if (--remaining === 0) return callback();
-        });
-    });
-}
