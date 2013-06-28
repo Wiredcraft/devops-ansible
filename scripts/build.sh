cb0f95ca7f77031bbb83e4e7a903cf0b2b12dade
Vincent Viallet <vincent@wiredcraft.com>
2013-06-28 13:15:11 +0800

    Add build script, build folder and .gitignore to .. ignore the packages that have been built.

---
+++ scripts/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+###########################
+# This build script will:
+#  - fetch the latest code
+#  - create tar files for each of the packages version
+###########################
+
+# Doesn't work in MacOS ...
+# CURRENT_FOLDER=$(readlink -f $(dirname $0))
+
+BUILD_FOLDER='../builds'
+
+cd $(dirname $0)
+mkdir -p $BUILD_FOLDER
+# Cleaning folder
+rm -rf $BUILD_FOLDER/*
+
+for package in $(ls -d ../packages/*/.); do
+    cd $package
+    for version in $(ls -d */.); do
+        PKG_VERSION=$(dirname $version)
+        PKG_FOLDER=$(dirname $package)
+        PKG_NAME=$(basename $(dirname $package))
+        tar cvfz ../$BUILD_FOLDER/${PKG_NAME}-${PKG_VERSION}.tar.gz ${PKG_VERSION} \
+            > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "[SUCCESS] $PKG_NAME - $PKG_VERSION: package created."
+        else
+            echo "[ERROR] $PKG_NAME - $PKG_VERSION: package creation failed."
+        fi
+    done
+    cd ..
+done
