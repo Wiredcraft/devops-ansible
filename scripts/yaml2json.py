712f874053e8e2b0e5a048b4ce550b6c3fa0d15b
Vincent Viallet <vincent@wiredcraft.com>
2013-07-06 21:01:16 +0800

    Add simple yaml2json script, update build-package to build providers as well, update deploy to deploy providers and start working on the API deploy strategy.

---
+++ scripts/yaml2json.py
@@ -0,0 +1,11 @@
+import sys
+import yaml
+import json
+
+try:
+    raw = sys.stdin.read()
+    data = json.dumps(yaml.safe_load(raw), indent=4)
+    print data
+except Exception, e:
+    print "you suck at yaml biatch - %s" % (e,)
+    sys.exit(1)
